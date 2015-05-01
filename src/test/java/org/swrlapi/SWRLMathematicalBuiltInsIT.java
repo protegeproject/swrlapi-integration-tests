3f6f92e3837721d4c67e465bc6a13e5f8b9b1e07
martinjoconnor <martin.oconnor@stanford.edu>
2015-05-01 09:34:13 -0700

    Bulk code reformat

--- src/test/java/org/swrlapi/SWRLMathematicalBuiltInsIT.java
+++ src/test/java/org/swrlapi/SWRLMathematicalBuiltInsIT.java
@@ -11,22 +11,22 @@ import org.swrlapi.test.SWRLAPIIntegrationTestBase;
 
 public class SWRLMathematicalBuiltInsIT extends SWRLAPIIntegrationTestBase
 {
-	@Before
-	public void setUp() throws OWLOntologyCreationException
-	{
-		createOWLOntologyAndSQWRLQueryEngine();
-	}
+  @Before
+  public void setUp() throws OWLOntologyCreationException
+  {
+    createOWLOntologyAndSQWRLQueryEngine();
+  }
 
-	@Test
-	public void TestSWRLCoreLessThanBuiltInWithShort() throws SWRLParseException, SQWRLException
-	{
-		declareOWLDataPropertyAssertion("p1", "hasAge", "42", "xsd:int");
+  @Test
+  public void TestSWRLCoreLessThanBuiltInWithShort() throws SWRLParseException, SQWRLException
+  {
+    declareOWLDataPropertyAssertion("p1", "hasAge", "42", "xsd:int");
 
-		SQWRLResult result = executeSQWRLQuery("q1",
-				"hasAge(p1, ?age) ^ swrlm:eval(?r, \"age + 1\", ?age) -> sqwrl:select(?r)");
+    SQWRLResult result = executeSQWRLQuery("q1",
+        "hasAge(p1, ?age) ^ swrlm:eval(?r, \"age + 1\", ?age) -> sqwrl:select(?r)");
 
-		Assert.assertTrue(result.next());
-		Assert.assertTrue(result.getLiteral("r").isDouble());
-		Assert.assertEquals(result.getLiteral("r").getDouble(), 43.0d, DELTA);
-	}
+    Assert.assertTrue(result.next());
+    Assert.assertTrue(result.getLiteral("r").isDouble());
+    Assert.assertEquals(result.getLiteral("r").getDouble(), 43.0d, this.DELTA);
+  }
 }
--- src/test/java/org/swrlapi/SWRLTemporalBuiltInsIT.java
+++ src/test/java/org/swrlapi/SWRLTemporalBuiltInsIT.java
@@ -12,74 +12,74 @@ import org.swrlapi.test.SWRLAPIIntegrationTestBase;
 
 public class SWRLTemporalBuiltInsIT extends SWRLAPIIntegrationTestBase
 {
-	@Before
-	public void setUp() throws OWLOntologyCreationException
-	{
-		createOWLOntologyAndSQWRLQueryEngine();
-	}
+  @Before
+  public void setUp() throws OWLOntologyCreationException
+  {
+    createOWLOntologyAndSQWRLQueryEngine();
+  }
 
-	@Test
-	public void TestSWRLTemporalBeforeBuiltIn() throws SWRLParseException, SQWRLException
-	{
-		SQWRLResult result = executeSQWRLQuery("q1", "temporal:before(\"01-01-10\", \"01-01-13\") -> sqwrl:select(0)");
+  @Test
+  public void TestSWRLTemporalBeforeBuiltIn() throws SWRLParseException, SQWRLException
+  {
+    SQWRLResult result = executeSQWRLQuery("q1", "temporal:before(\"01-01-10\", \"01-01-13\") -> sqwrl:select(0)");
 
-		Assert.assertTrue(result.next());
-	}
+    Assert.assertTrue(result.next());
+  }
 
-	@Test
-	public void TestSWRLTemporalAfterBuiltIn() throws SWRLParseException, SQWRLException
-	{
-		SQWRLResult result = executeSQWRLQuery("q1", "temporal:after(\"01-01-13\", \"01-01-10\") -> sqwrl:select(0)");
+  @Test
+  public void TestSWRLTemporalAfterBuiltIn() throws SWRLParseException, SQWRLException
+  {
+    SQWRLResult result = executeSQWRLQuery("q1", "temporal:after(\"01-01-13\", \"01-01-10\") -> sqwrl:select(0)");
 
-		Assert.assertTrue(result.next());
-	}
+    Assert.assertTrue(result.next());
+  }
 
-	@Test
-	public void TestSWRLTemporalDurationBuiltIn() throws SWRLParseException, SQWRLException
-	{
-		SQWRLResult result = executeSQWRLQuery("q1",
-				"temporal:duration(?r, \"1999-11-01T10:00:01.3\", \"2000-19-01T10:00:01.3\", \"Years\") -> sqwrl:select(?r)");
+  @Test
+  public void TestSWRLTemporalDurationBuiltIn() throws SWRLParseException, SQWRLException
+  {
+    SQWRLResult result = executeSQWRLQuery("q1",
+        "temporal:duration(?r, \"1999-11-01T10:00:01.3\", \"2000-19-01T10:00:01.3\", \"Years\") -> sqwrl:select(?r)");
 
-		Assert.assertTrue(result.next());
-		Assert.assertTrue(result.getLiteral("r").isLong());
-		Assert.assertEquals(result.getLiteral("r").getLong(), 1);
-	}
+    Assert.assertTrue(result.next());
+    Assert.assertTrue(result.getLiteral("r").isLong());
+    Assert.assertEquals(result.getLiteral("r").getLong(), 1);
+  }
 
-	@Test
-	public void TestSWRLTemporalEqualsBuiltIn() throws SWRLParseException, SQWRLException
-	{
-		SQWRLResult result = executeSQWRLQuery("q1",
-				"temporal:equals(\"1999-11-01T10:00:01.3\", \"1999-11-01T10:00:01.3\", \"Years\") -> sqwrl:select(0)");
+  @Test
+  public void TestSWRLTemporalEqualsBuiltIn() throws SWRLParseException, SQWRLException
+  {
+    SQWRLResult result = executeSQWRLQuery("q1",
+        "temporal:equals(\"1999-11-01T10:00:01.3\", \"1999-11-01T10:00:01.3\", \"Years\") -> sqwrl:select(0)");
 
-		Assert.assertTrue(result.next());
-	}
+    Assert.assertTrue(result.next());
+  }
 
-	@Test
-	public void TestSWRLTemporalEqualsBuiltInYearsGranularity() throws SWRLParseException, SQWRLException
-	{
-		SQWRLResult result = executeSQWRLQuery("q1",
-				"temporal:equals(\"1999-10-10T11:10:22.1\", \"1999-11-01T10:00:01.3\", \"Years\") -> sqwrl:select(0)");
+  @Test
+  public void TestSWRLTemporalEqualsBuiltInYearsGranularity() throws SWRLParseException, SQWRLException
+  {
+    SQWRLResult result = executeSQWRLQuery("q1",
+        "temporal:equals(\"1999-10-10T11:10:22.1\", \"1999-11-01T10:00:01.3\", \"Years\") -> sqwrl:select(0)");
 
-		Assert.assertTrue(result.next());
-	}
+    Assert.assertTrue(result.next());
+  }
 
-	@Test
-	public void TestSWRLTemporalEqualsBuiltInMonthsGranularity() throws SWRLParseException, SQWRLException
-	{
-		SQWRLResult result = executeSQWRLQuery("q1",
-				"temporal:equals(\"1999-10-10T11:10:22.1\", \"1999-10-01T10:00:01.3\", \"Years\") -> sqwrl:select(0)");
+  @Test
+  public void TestSWRLTemporalEqualsBuiltInMonthsGranularity() throws SWRLParseException, SQWRLException
+  {
+    SQWRLResult result = executeSQWRLQuery("q1",
+        "temporal:equals(\"1999-10-10T11:10:22.1\", \"1999-10-01T10:00:01.3\", \"Years\") -> sqwrl:select(0)");
 
-		Assert.assertTrue(result.next());
-	}
+    Assert.assertTrue(result.next());
+  }
 
-	@Test
-	public void TestSWRLTemporalAddBuiltIn() throws SWRLParseException, SQWRLException
-	{
-		SQWRLResult result = executeSQWRLQuery("q1",
-				"temporal:add(?r, \"1999-11-01T10:00\", 4, \"Years\") -> sqwrl:select(?r)");
+  @Test
+  public void TestSWRLTemporalAddBuiltIn() throws SWRLParseException, SQWRLException
+  {
+    SQWRLResult result = executeSQWRLQuery("q1",
+        "temporal:add(?r, \"1999-11-01T10:00\", 4, \"Years\") -> sqwrl:select(?r)");
 
-		Assert.assertTrue(result.next());
-		Assert.assertTrue(result.getLiteral("r").isDateTime());
-		Assert.assertEquals(result.getLiteral("r").getDateTime(), new XSDDateTime("2003-11-01T10:00:00.0"));
-	}
+    Assert.assertTrue(result.next());
+    Assert.assertTrue(result.getLiteral("r").isDateTime());
+    Assert.assertEquals(result.getLiteral("r").getDateTime(), new XSDDateTime("2003-11-01T10:00:00.0"));
+  }
 }
