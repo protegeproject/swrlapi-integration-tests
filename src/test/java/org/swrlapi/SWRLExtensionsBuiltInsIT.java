141d3608b63828ab639aa1eb101ef22dfe17d289
martin <martin@zippyrate.com>
2016-05-30 10:57:24 -0700

    Initial SWRL Extensions built-in test

---
+++ src/test/java/org/swrlapi/SWRLExtensionsBuiltInsIT.java
@@ -0,0 +1,37 @@
+package org.swrlapi;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.OWLNamedIndividual;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+import org.swrlapi.factory.SWRLAPIFactory;
+import org.swrlapi.parser.SWRLParseException;
+import org.swrlapi.sqwrl.SQWRLQueryEngine;
+import org.swrlapi.sqwrl.SQWRLResult;
+import org.swrlapi.sqwrl.exceptions.SQWRLException;
+import org.swrlapi.test.IntegrationTestBase;
+
+import static org.semanticweb.owlapi.apibinding.OWLFunctionalSyntaxFactory.ClassAssertion;
+import static org.semanticweb.owlapi.apibinding.OWLFunctionalSyntaxFactory.NamedIndividual;
+import static org.semanticweb.owlapi.apibinding.OWLFunctionalSyntaxFactory.OWLThing;
+
+public class SWRLExtensionsBuiltInsIT extends IntegrationTestBase
+{
+  private static final OWLNamedIndividual P1 = NamedIndividual(iri("p1"));
+
+  @Test public void TestSWRLExtensionsMakeOWLThingBuiltIn()
+    throws SWRLParseException, SQWRLException, OWLOntologyCreationException
+  {
+    OWLOntology ontology = OWLManager.createOWLOntologyManager().createOntology();
+    SQWRLQueryEngine queryEngine = SWRLAPIFactory.createSQWRLQueryEngine(ontology);
+
+    addOWLAxioms(ontology, ClassAssertion(OWLThing(), P1));
+
+    SQWRLResult result = queryEngine
+      .runSQWRLQuery("q1", "owl:Thing(?x) ^ swrlx:makeOWLThing(?t, ?x) -> sqwrl:select(?t)");
+
+    Assert.assertTrue(result.next());
+  }
+}
