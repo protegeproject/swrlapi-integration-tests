d94ac8cece8b7f64a216ff4d412390eb87badb11
Martin O'Connor <martin@Martins-iMac.local>
2020-06-20 16:40:35 -0700

    Added more mathematical built-in tests

---
+++ src/test/java/org/swrlapi/SWRLCoreMathBuiltInsIT.java
@@ -0,0 +1,164 @@
+package org.swrlapi;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+import org.swrlapi.factory.SWRLAPIFactory;
+import org.swrlapi.parser.SWRLParseException;
+import org.swrlapi.sqwrl.SQWRLQueryEngine;
+import org.swrlapi.sqwrl.SQWRLResult;
+import org.swrlapi.sqwrl.exceptions.SQWRLException;
+import org.swrlapi.test.IntegrationTestBase;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+/**
+ * NOTE: All tests are designed for parallel execution.
+ *
+ * @see org.swrlapi.builtins.swrlb.SWRLBuiltInLibraryImpl
+ */
+public class SWRLCoreMathBuiltInsIT extends IntegrationTestBase
+{
+  @Test public void TestSWRLCoreMathAddBuiltInWithByte()
+    throws SWRLParseException, SQWRLException, OWLOntologyCreationException
+  {
+    OWLOntology ontology = OWLManager.createOWLOntologyManager().createOntology();
+    SQWRLQueryEngine queryEngine = SWRLAPIFactory.createSQWRLQueryEngine(ontology);
+
+    String query = "swrlb:add(?r, \"2\"^^xsd:byte, \"2\"^^xsd:byte) -> sqwrl:select(?r)";
+    SQWRLResult result = queryEngine.runSQWRLQuery("q1", query);
+
+    Assert.assertTrue(result.next());
+    Assert.assertTrue(result.getLiteral("r").isByte());
+    Assert.assertEquals(4, result.getLiteral("r").getByte());
+  }
+
+  @Test public void TestSWRLCoreMathAddBuiltInWithShort()
+    throws SWRLParseException, SQWRLException, OWLOntologyCreationException
+  {
+    OWLOntology ontology = OWLManager.createOWLOntologyManager().createOntology();
+    SQWRLQueryEngine queryEngine = SWRLAPIFactory.createSQWRLQueryEngine(ontology);
+
+    String query = "swrlb:add(?r, \"2\"^^xsd:short, \"2\"^^xsd:short) -> sqwrl:select(?r)";
+    SQWRLResult result = queryEngine.runSQWRLQuery("q1", query);
+
+    Assert.assertTrue(result.next());
+    Assert.assertTrue(result.getLiteral("r").isShort());
+    Assert.assertEquals(4, result.getLiteral("r").getShort());
+  }
+
+  @Test public void TestSWRLCoreMathAddBuiltInWithInt()
+    throws SWRLParseException, SQWRLException, OWLOntologyCreationException
+  {
+    OWLOntology ontology = OWLManager.createOWLOntologyManager().createOntology();
+    SQWRLQueryEngine queryEngine = SWRLAPIFactory.createSQWRLQueryEngine(ontology);
+
+    SQWRLResult result = queryEngine
+      .runSQWRLQuery("q1", "swrlb:add(?r, \"2\"^^xsd:int, \"2\"^^xsd:int) -> sqwrl:select(?r)");
+
+    Assert.assertTrue(result.next());
+    Assert.assertTrue(result.getLiteral("r").isInt());
+    Assert.assertEquals(4, result.getLiteral("r").getInt());
+  }
+
+  @Test public void TestSWRLCoreMathAddBuiltInWithInteger()
+    throws SWRLParseException, SQWRLException, OWLOntologyCreationException
+  {
+    OWLOntology ontology = OWLManager.createOWLOntologyManager().createOntology();
+    SQWRLQueryEngine queryEngine = SWRLAPIFactory.createSQWRLQueryEngine(ontology);
+
+    SQWRLResult result = queryEngine.runSQWRLQuery("q1", "swrlb:add(?r, 2, 2) -> sqwrl:select(?r)");
+
+    Assert.assertTrue(result.next());
+    Assert.assertTrue(result.getLiteral("r").isInteger());
+    Assert.assertEquals(BigInteger.valueOf(4), result.getLiteral("r").getInteger());
+  }
+
+  @Test public void TestSWRLCoreMathAddBuiltInWithFloat()
+    throws SWRLParseException, SQWRLException, OWLOntologyCreationException
+  {
+    OWLOntology ontology = OWLManager.createOWLOntologyManager().createOntology();
+    SQWRLQueryEngine queryEngine = SWRLAPIFactory.createSQWRLQueryEngine(ontology);
+
+    SQWRLResult result = queryEngine
+      .runSQWRLQuery("q1", "swrlb:add(?r, \"2.1\"^^xsd:float, \"2.0\"^^xsd:float) -> sqwrl:select(?r)");
+
+    Assert.assertTrue(result.next());
+    Assert.assertTrue(result.getLiteral("r").isFloat());
+    Assert.assertEquals(4.1f, result.getLiteral("r").getFloat(), IntegrationTestBase.DELTA);
+  }
+
+  @Test public void TestSWRLCoreMathAddBuiltInWithLong()
+    throws SWRLParseException, SQWRLException, OWLOntologyCreationException
+  {
+    OWLOntology ontology = OWLManager.createOWLOntologyManager().createOntology();
+    SQWRLQueryEngine queryEngine = SWRLAPIFactory.createSQWRLQueryEngine(ontology);
+
+    String query = "swrlb:add(?r, \"2\"^^xsd:long, \"2\"^^xsd:long) -> sqwrl:select(?r)";
+    SQWRLResult result = queryEngine.runSQWRLQuery("q1", query);
+
+    Assert.assertTrue(result.next());
+    Assert.assertTrue(result.getLiteral("r").isLong());
+    Assert.assertEquals(4l, result.getLiteral("r").getLong());
+  }
+
+  @Test public void TestSWRLCoreMathAddBuiltInWithDoubles()
+    throws SWRLParseException, SQWRLException, OWLOntologyCreationException
+  {
+    OWLOntology ontology = OWLManager.createOWLOntologyManager().createOntology();
+    SQWRLQueryEngine queryEngine = SWRLAPIFactory.createSQWRLQueryEngine(ontology);
+
+    String query = "swrlb:add(?r, \"2.0\"^^xsd:double, \"2.0\"^^xsd:double)" + " -> sqwrl:select(?r)";
+    SQWRLResult result = queryEngine.runSQWRLQuery("q1", query);
+
+    Assert.assertTrue(result.next());
+    Assert.assertTrue(result.getLiteral("r").isDouble());
+    Assert.assertEquals(4.0d, result.getLiteral("r").getDouble(), IntegrationTestBase.DELTA);
+  }
+
+  @Test public void TestSWRLCoreMathAddBuiltInWithDecimals()
+    throws SWRLParseException, SQWRLException, OWLOntologyCreationException
+  {
+    OWLOntology ontology = OWLManager.createOWLOntologyManager().createOntology();
+    SQWRLQueryEngine queryEngine = SWRLAPIFactory.createSQWRLQueryEngine(ontology);
+
+    String query = "swrlb:add(?r, \"2.0\"^^xsd:decimal, \"2.0\"^^xsd:decimal)" + " -> sqwrl:select(?r)";
+    SQWRLResult result = queryEngine.runSQWRLQuery("q1", query);
+
+    Assert.assertTrue(result.next());
+    Assert.assertTrue(result.getLiteral("r").isDecimal());
+    Assert.assertEquals(BigDecimal.valueOf(4.0), result.getLiteral("r").getDecimal());
+  }
+
+  @Test public void TestSWRLCoreMathSinBuiltInWithDecimals()
+    throws SWRLParseException, SQWRLException, OWLOntologyCreationException
+  {
+    OWLOntology ontology = OWLManager.createOWLOntologyManager().createOntology();
+    SQWRLQueryEngine queryEngine = SWRLAPIFactory.createSQWRLQueryEngine(ontology);
+
+    String query = "swrlb:sin(?r, \"0\"^^xsd:decimal)" + " -> sqwrl:select(?r)";
+    SQWRLResult result = queryEngine.runSQWRLQuery("q1", query);
+
+    Assert.assertTrue(result.next());
+    Assert.assertTrue(result.getLiteral("r").isDecimal());
+    Assert.assertEquals(BigDecimal.ZERO, result.getLiteral("r").getDecimal());
+  }
+
+  @Test public void TestSWRLCoreMathTanBuiltInWithDecimals()
+    throws SWRLParseException, SQWRLException, OWLOntologyCreationException
+  {
+    OWLOntology ontology = OWLManager.createOWLOntologyManager().createOntology();
+    SQWRLQueryEngine queryEngine = SWRLAPIFactory.createSQWRLQueryEngine(ontology);
+
+    String query = "swrlb:cos(?r, \"0\"^^xsd:decimal)" + " -> sqwrl:select(?r)";
+    SQWRLResult result = queryEngine.runSQWRLQuery("q1", query);
+
+    Assert.assertTrue(result.next());
+    Assert.assertTrue(result.getLiteral("r").isDecimal());
+    Assert.assertEquals(BigDecimal.ONE, result.getLiteral("r").getDecimal());
+  }
+
+}
--- src/test/java/org/swrlapi/SWRLMathematicalBuiltInsIT.java
+++ src/test/java/org/swrlapi/SWRLMathematicalBuiltInsIT.java
@@ -7,12 +7,15 @@ import org.semanticweb.owlapi.model.OWLDataProperty;
 import org.semanticweb.owlapi.model.OWLNamedIndividual;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+import org.semanticweb.owlapi.vocab.OWL2Datatype;
 import org.swrlapi.factory.SWRLAPIFactory;
 import org.swrlapi.parser.SWRLParseException;
 import org.swrlapi.sqwrl.SQWRLQueryEngine;
 import org.swrlapi.sqwrl.SQWRLResult;
 import org.swrlapi.sqwrl.exceptions.SQWRLException;
 import org.swrlapi.test.IntegrationTestBase;
+
+import java.math.BigDecimal;
 
 import static org.semanticweb.owlapi.apibinding.OWLFunctionalSyntaxFactory.DataProperty;
 import static org.semanticweb.owlapi.apibinding.OWLFunctionalSyntaxFactory.DataPropertyAssertion;
@@ -27,21 +30,57 @@ import static org.semanticweb.owlapi.apibinding.OWLFunctionalSyntaxFactory.Named
 public class SWRLMathematicalBuiltInsIT extends IntegrationTestBase
 {
   private static final OWLNamedIndividual P1 = NamedIndividual(iri("p1"));
-  private static final OWLDataProperty HAS_AGE = DataProperty(iri("hasAge"));
+  private static final OWLDataProperty HAS_HEIGHT = DataProperty(iri("hasHeight"));
 
-  @Test public void TestSWRLCoreLessThanBuiltInWithShort()
+  @Test public void TestSWRLMathematicalLogBuiltIn()
     throws SWRLParseException, SQWRLException, OWLOntologyCreationException
   {
     OWLOntology ontology = OWLManager.createOWLOntologyManager().createOntology();
     SQWRLQueryEngine queryEngine = SWRLAPIFactory.createSQWRLQueryEngine(ontology);
 
-    addOWLAxioms(ontology, DataPropertyAssertion(HAS_AGE, P1, Literal("42", XSD_INT)));
+    addOWLAxioms(ontology, DataPropertyAssertion(HAS_HEIGHT, P1, Literal("100", OWL2Datatype.XSD_DECIMAL)));
 
     SQWRLResult result = queryEngine
-      .runSQWRLQuery("q1", "hasAge(p1, ?age) ^ swrlm:eval(?r, \"age + 1\", ?age) -> sqwrl:select(?r)");
+      .runSQWRLQuery("q1", "hasHeight(p1, ?height) ^ swrlm:log(?r, ?height) -> sqwrl:select(?r)");
+
+    Assert.assertTrue(result.next());
+    Assert.assertTrue(result.getLiteral("r").isDecimal());
+    Assert.assertEquals(BigDecimal.valueOf(2), result.getLiteral("r").getDecimal());
+  }
+
+  @Test public void TestSWRLMathematicalSqrtBuiltIn()
+    throws SWRLParseException, SQWRLException, OWLOntologyCreationException
+  {
+    OWLOntology ontology = OWLManager.createOWLOntologyManager().createOntology();
+    SQWRLQueryEngine queryEngine = SWRLAPIFactory.createSQWRLQueryEngine(ontology);
+
+    addOWLAxioms(ontology, DataPropertyAssertion(HAS_HEIGHT, P1, Literal("100", OWL2Datatype.XSD_DECIMAL)));
+
+    SQWRLResult result = queryEngine
+      .runSQWRLQuery("q1", "hasHeight(p1, ?height) ^ swrlm:sqrt(?r, ?height) -> sqwrl:select(?r)");
+
+    Assert.assertTrue(result.next());
+    Assert.assertTrue(result.getLiteral("r").isDecimal());
+    Assert.assertEquals(BigDecimal.valueOf(10.0), result.getLiteral("r").getDecimal());
+  }
+
+  @Test public void TestSWRLMathematicalEvalBuiltIn()
+    throws SWRLParseException, SQWRLException, OWLOntologyCreationException
+  {
+    OWLOntology ontology = OWLManager.createOWLOntologyManager().createOntology();
+    SQWRLQueryEngine queryEngine = SWRLAPIFactory.createSQWRLQueryEngine(ontology);
+
+    addOWLAxioms(ontology, DataPropertyAssertion(HAS_HEIGHT, P1, Literal("100", OWL2Datatype.XSD_DOUBLE)));
+
+    SQWRLResult result = queryEngine
+      .runSQWRLQuery("q1", "hasHeight(p1, ?height) ^ swrlm:eval(?r, \"height + 1\", ?height) -> sqwrl:select(?r)");
 
     Assert.assertTrue(result.next());
     Assert.assertTrue(result.getLiteral("r").isDouble());
-    Assert.assertEquals(43.0d, result.getLiteral("r").getDouble(), IntegrationTestBase.DELTA);
+    Assert.assertEquals(Double.valueOf("101"), result.getLiteral("r").getDouble(), IntegrationTestBase.DELTA);
   }
+
 }
+
+//  SQWRLResult result = queryEngine
+//    .runSQWRLQuery("q1", "hasAge(p1, ?age) ^ swrlm:eval(?r, \"age + 1\", ?age) -> sqwrl:select(?r)");
